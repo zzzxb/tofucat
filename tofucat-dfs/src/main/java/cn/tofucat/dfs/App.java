0baf5584576d42e74d8186a93d8e88d401100833
Zzzxb <idearfree@outlook.com>
2025-04-25 16:54:11 +0800

    初始化两个项目

---
+++ tofucat-dfs/src/main/java/cn/tofucat/dfs/App.java
@@ -0,0 +1,12 @@
+package cn.tofucat.dfs;
+
+/**
+ *
+ * @author zzzxb
+ * 2025/4/25
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("文件管理系统");
+    }
+}
---
+++ tofucat-wbs/src/main/java/cn/tofucat/wbs/App.java
@@ -0,0 +1,17 @@
+package cn.tofucat.wbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author zzzxb
+ * 2025/4/25
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
